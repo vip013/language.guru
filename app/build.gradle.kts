ad6c783e609b12eb9f74f50f442a692b28f66885
vip <vishnu.vip010@gmail.com>
2026-08-07 22:17:30 +0530

    build: update signing config and gradle wrapper
    Switch release build configuration to use debug signing and update the Gradle wrapper to version 9.3.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
       isCrunchPngs = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-      signingConfig = signingConfigs.getByName("release")
+      signingConfig = signingConfigs.getByName("debugConfig")
     }
     debug {
       signingConfig = signingConfigs.getByName("debugConfig")
